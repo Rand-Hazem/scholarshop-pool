7195d173eb06371f440bd70bfa847f196f695a4e
Rand <rand.hazem03@hotmail.com>
2019-11-13 01:17:15 +0200

    try to get a successful request to api, but dosn't work, some change are important

--- src/main/resources/static/js/shared.js
+++ src/main/resources/static/js/shared.js
@@ -1,26 +1,30 @@
 function universityAutoComplete() {
     var inputItem = $("input[name='university']");
-    // url: 'http://universities.hipolabs.com/search?name=' + $(inputItem).val(),
     $("input[name='university']").autocomplete({
         source: function (request, response) {
             $.ajax({
-                url: '/user/test',
+                url: 'http://universities.hipolabs.com/search?name=' + $(inputItem).val(),
+                // url: '/user/test',
                 type: 'get',
                 crossDomain: true,
+                async:true,
+                dataType : 'jsonp',
                 headers: {'Access-Control-Allow-Origin': '*'},
                 beforeSend: function () {
-                    // console.log("befre " + $(inputItem).val());
+                    console.log("befre " + $(inputItem).val());
                 },
                 success: function (data) {
-                    var list = new Array();
                     console.log(data);
-                    // var list = new Array();
-                    // $.each(data, function (i, item) {
-                    //     list.push(item.name + " / " + item.country);
-                    // })
-                    list.push("akjsnd")
-                    list.push("aomgk")
+                    var list = new Array();
+                    var list = new Array();
+                    $.each(data, function (i, item) {
+                        list.push(item.name + " / " + item.country);
+                    });
                     response(list);
+                },error: function (jqXHR, textStatus, errorThrown) {
+                    console.log(textStatus);
+                    console.log(errorThrown)
+                    console.log(jqXHR)
                 }
             });
         }
